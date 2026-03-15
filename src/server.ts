25f3acf811d52b9014935b9d530c729dc04ec4cc
hagit-gh <hagitshimelman@gmail.com>
2026-03-15 09:05:57 +0200

    Add server file

---
+++ src/server.ts
@@ -0,0 +1,56 @@
+import express, { type Request, type Response } from "express";
+import { runGetEobData } from "./automation/eob";
+import { runSendAppealFlow } from "./automation/appeal";
+import { randomUUID } from "crypto";
+import { AsyncLocalStorage } from "async_hooks";
+import { generateEOBResponse } from "./automation/eob";
+
+export const requestContext = new AsyncLocalStorage<Map<string, any>>();
+export const ROOT_DIR = process.cwd();
+
+const PORT = 3000;
+const CSV_FILE = `${ROOT_DIR}\\payer_targets.csv`
+
+const app = express();
+app.use(express.json()); 
+
+app.use((req, res, next) => {
+    const store = new Map();
+    const runId = `${new Date().toISOString().replace(/[:.]/g, "-")}-${randomUUID().substring(0, 4).toUpperCase()}`
+    store.set("runId", runId);
+    store.set("downloadsPath", `${ROOT_DIR}\\artifacts\\${runId}`);
+
+    requestContext.run(store, () => {
+        next();
+    });
+});
+
+// Endpoint 1: Retrieve EOBs
+app.post("/api/payer/eobs", async (req: Request, res: Response) => {
+    try {
+        const data = req.body;
+        let results = await runGetEobData(CSV_FILE, data);
+        res.setHeader("Content-Type", "application/json");
+        res.json(generateEOBResponse(results, data));
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+// Endpoint 2: Submit Appeal
+app.post("/api/payer/submit-appeal", async (req: Request, res: Response) => {
+    try {
+        const data = req.body; // { payerId, taxId, claimId, appealType, attachments, submit, ... }
+        const result = await runSendAppealFlow(CSV_FILE, data);
+        res.json(result);
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
